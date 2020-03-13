84684cab7b4ee7f0a11b76461e88d302da52e819
Jenfi <jennifer_fick@hotmail.com>
2020-03-13 12:11:36 +0100

    first commit: added routing in app.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { BrowserRouter, Route, Switch, Link } from 'react-router-dom'
+import { Header } from './components/Header'
+import { Hero } from './components/Hero'
+import { Home } from './pages/Home'
+import { Flower } from './pages/Flower'
+// import { ReactComponent as Arrow } from './attributes/arrow.svg'
+import './styling/hero.css'
+
+import './styling/app.css'
 
 export const App = () => {
+  // const [showFlowers, setShowFlowers] = useState(false)
+
   return (
-    <div>
-      Find me in src/app.js!
-    </div>
+    <BrowserRouter>
+      <article>
+        <Header />
+        <Hero />
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          {/* <Route exact path="/flower/:flowerId"> */}
+          <Route exact path="/flower/:index">
+            <Flower />
+          </Route>
+        </Switch>
+      </article>
+    </BrowserRouter>
   )
 }
