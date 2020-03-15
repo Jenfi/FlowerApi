e5c754ca79616361a91188e214c2d4e2c32cee1b
Jenfi <jennifer_fick@hotmail.com>
2020-03-15 17:55:26 +0100

    changed Hero

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,12 +1,12 @@
 import React from 'react'
-import hero from '../attributes/hero.jpg'
+import hero2 from '../attributes/hero2.jpg'
 import '../styling/hero.css'
 
 export const Hero = () => {
 
   return (
     <section className="hero-container">
-      <div className="hero" style={{ backgroundImage: `url(${hero})` }} />
+      <div className="hero" style={{ backgroundImage: `url(${hero2})` }} />
       {/* <h1> We've gathered all your favourite flowers</h1> */}
 
     </section>
