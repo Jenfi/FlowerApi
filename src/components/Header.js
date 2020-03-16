03af84dca0fcf8b2f3eadc5f4c5b9166ed2a731d
Jenfi <jennifer_fick@hotmail.com>
2020-03-16 09:23:20 +0100

    added animation to header h1

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,7 @@ export const Header = () => {
   return (
     <Route path="/">
       <header>
-        <Link to="/"><h1>FLOWERHUB</h1></Link>
+        <Link to="/"><h1 className="tracking-in-expand-fwd">FLOWERHUB</h1></Link>
       </header>
     </Route>
   )
